55e0bfb80b78790cb0e81b305ac5e6fe09a269e4
palmbeach05 <5945226+palmbeach05@users.noreply.github.com>
2026-06-09 07:40:59 -0400

    j5ltechn: Cleanup
    Overlay was removed a while ago, somehow this line was missed. And some
    lines that were commented out can be removed, since they are not being
    used anyways. Also, RIL values are not needed for TWRP, and
    omni.dependencies are present in msm8916-common, j5-common, and in each
    variant's directory. Let's get rid of the redundant duplicity to
    eliminate any dependency conflicts, synchronization headaches, and
    redundant manifest parsing during workspace syncs.

--- system_prop.mk
+++
@@ -1,9 +0,0 @@
-# RIL
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	telephony.lteOnCdmaDevice=0 \
-	persist.radio.tdscdma_present=1 \
-	ro.telephony.default_network=9 \
-	ro.multisim.simslotcount=2 \
-	ro.telephony.ril.config=simactivation \
-	persist.radio.multisim.config=dsds \
-	rild.libpath2=/system/lib/libsec-ril-dsds.so
