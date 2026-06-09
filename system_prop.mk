fd5eb8c3bdaabf1a25127512da44bb3353db78a0
palmbeach05 <5945226+palmbeach05@users.noreply.github.com>
2026-06-09 07:35:55 -0400

    j5ltechn: Move system_prop values to j5-common
    The values that were in system_prop.mk are common amongst all of the
    j5 2015 devices. It would be more appropriate to have them moved to
    j5-common.

--- system_prop.mk
+++ system_prop.mk
@@ -1,11 +1,3 @@
-# CABL
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.qualcomm.cabl=2
-
-# RAM
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.config.low_ram=false
-
 # RIL
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	telephony.lteOnCdmaDevice=0 \
